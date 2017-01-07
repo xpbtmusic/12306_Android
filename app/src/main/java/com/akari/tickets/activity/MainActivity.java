ff0c74f85af0a12274a29a6cbcd5c3c633fdaa46
akari10032 <akari10032@gmail.com>
2017-01-07 14:05:31 +0800

    v0.11

--- app/src/main/java/com/akari/tickets/activity/MainActivity.java
+++ app/src/main/java/com/akari/tickets/activity/MainActivity.java
@@ -5,14 +5,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -39,10 +35,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -482,5 +475,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 manager.notify(1, notification);
             }
         }).start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
